2203568b6c0f412e907e268089444fef2f1a1fc1
Andriy Ermolenko <andriy.ermolenko@gmail.com>
2017-08-03 04:37:44 +0300

    Order items server fix

--- modules/data/server/models/order.server.model.js
+++ modules/data/server/models/order.server.model.js
@@ -112,6 +112,7 @@ OrderSchema.pre('save', function (next) {
       }
     }
   ], () => {
+    this.items || (this.items = []);
     const functions = this.items.map(item => {
       return (next) => {
         const id = item.good._id || item.good;
