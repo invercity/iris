3ef60fce43b8d21c9d94ac4529c9e3d634e12098
Andrii Yermolenko <andriy.ermolenko@gmail.com>
2019-03-21 23:11:44 +0200

    Added mongo connect options

--- config/env/production.js
+++ config/env/production.js
@@ -9,7 +9,8 @@ module.exports = {
     uri: process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://' + (process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost') + '/mean',
     options: {
       user: '',
-      pass: ''
+      pass: '',
+      useNewUrlParser: true
     },
     // Enable mongoose debug mode
     debug: process.env.MONGODB_DEBUG || false
