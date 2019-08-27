a8d8c2401c3eb84ba607abe6986001054a82b91e
Andrii Yermolenko <andriy.ermolenko@gmail.com>
2019-08-27 18:28:38 +0300

    Changed HTML title

--- config/env/default.js
+++ config/env/default.js
@@ -1,6 +1,6 @@
 module.exports = {
   app: {
-    title: 'IRIS',
+    title: 'Склад v1.3.0',
     description: 'Full-Stack JavaScript with MongoDB, Express, AngularJS, and Node.js',
     keywords: 'mongodb, express, angularjs, node.js, mongoose, passport',
     googleAnalyticsTrackingID: process.env.GOOGLE_ANALYTICS_TRACKING_ID || 'GOOGLE_ANALYTICS_TRACKING_ID'
