39f96c66167fa93c562477353f94a2a5ad0ba8bc
Andrii Yermolenko <andriy.ermolenko@gmail.com>
2021-10-11 23:13:14 +0300

    #14 Get hostname on UI for links

--- modules/data/client/controllers/orders-edit.controller.js
+++ modules/data/client/controllers/orders-edit.controller.js
@@ -122,7 +122,7 @@ angular.module('data').controller('OrdersEditController', [
           $scope.calcArray = calcArray;
           $scope.savedOrder = _.cloneDeep(data);
           $scope.title = $scope.t.EDIT_ORDER_NUM + data.code;
-          $scope.order.link = 'https://vitaly.herokuapp.com/orders/' + data._id;
+          $scope.order.link = window.location.hostname + '/orders/' + data._id;
           if (!$scope.order.status) {
             $scope.order.status = $scope.statuses[0].value;
           }
